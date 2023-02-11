9b5fc1a723d74d6707f2e3b372cdeb802a4016b8
Muhammad Aqib <msbohw@gmail.com>
2023-02-12 00:57:49 +0500

    initial commit

--- lib/Views/chat_bot.dart
+++ lib/Views/chat_bot.dart
@@ -289,29 +289,28 @@ class ChatMessageWidget extends StatelessWidget {
   }
 }
 
-Future<String> generateResponse(String prompt) async {
-  const apiKey = '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';
+// Future<String> generateResponse(String prompt) async {
 
-  var url = Uri.https("api.openai.com", "/v1/completions");
-  final response = await http.post(
-    url,
-    headers: {
-      'Content-Type': 'application/json',
-      "Authorization": "Bearer $apiKey"
-    },
-    body: json.encode({
-      "model": "text-davinci-003",
-      "prompt": prompt,
-      'temperature': 0,
-      'max_tokens': 2000,
-      'top_p': 1,
-      'frequency_penalty': 0.0,
-      'presence_penalty': 0.0,
-    }),
-  );
+//   var url = Uri.https("api.openai.com", "/v1/completions");
+//   final response = await http.post(
+//     url,
+//     headers: {
+//       'Content-Type': 'application/json',
+//       "Authorization": "Bearer $apiKey"
+//     },
+//     body: json.encode({
+//       "model": "text-davinci-003",
+//       "prompt": prompt,
+//       'temperature': 0,
+//       'max_tokens': 2000,
+//       'top_p': 1,
+//       'frequency_penalty': 0.0,
+//       'presence_penalty': 0.0,
+//     }),
+//   );
 
-  // Do something with the response
-  Map<String, dynamic> newresponse = jsonDecode(response.body);
+//   // Do something with the response
+//   Map<String, dynamic> newresponse = jsonDecode(response.body);
 
-  return newresponse['choices'][0]['text'];
-}
+//   return newresponse['choices'][0]['text'];
+// }
